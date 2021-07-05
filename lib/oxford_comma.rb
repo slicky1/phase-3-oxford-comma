ac9d505a84ce57daf816676287e022235ee8eb3a
slicky1 <okunonke@gmail.com>
2021-07-05 16:30:16 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
-end
+    len = array.size
+    returns = array[0]
+    case len
+    when 1
+      returns
+    when 2
+      returns = "#{array[0]} and #{array[1]}"
+    else
+      i = 1;
+      (len-2).times do
+        returns = "#{returns}, #{array[i]}"
+        i += 1
+      end
+      returns = "#{returns}, and #{array[len-1]}"
+    end
+  end
